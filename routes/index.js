282ba493c6854a01d8f7e5d450e1406731b54a4d
kaztro <00229017@uca.edu.sv>
2019-04-09 12:41:12 -0600

    Dandole

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+'use strict'
+
+var mongoose = require('mongoose');
+var app = require('./app')
+
+var port = 3800;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://kaztro:A123456@ds129560.mlab.com:29560/pdm_taller_2', {
+  useMongoClient: true}).then(() => {
+    console.log('Buenale')
+    app.listen(port, () => {
+      console.log('Servidor corriendo xd');
+    });
+  }).catch(err => console.log(err));
+
+/*var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+*/
