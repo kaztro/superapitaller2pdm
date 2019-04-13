cdfe29b2d5b640b466976d3bfe44918efb217556
kaztro <00229017@uca.edu.sv>
2019-04-13 00:09:51 -0600

    Le faltan retoques xd

--- routes/coin.js
+++ routes/coin.js
@@ -8,19 +8,18 @@ var express = require('express'),
 router.post('/', CoinController.create);
 
 // Read
-router.get('/coin', CoinController.getAll);
-router.get('/id/:id', CoinController.getId);
-router.get('/name/:name', CoinController.getName);
-router.get('/country/:country', CoinController.getCountry);
-router.get('/value/:value', CoinController.getValue);
-router.get('/value_us/:value_us', CoinController.getVU);
-router.get('/year/:year', CoinController.getYear);
-router.get('/review/:review', CoinController.getReview);
-router.get('/available/:isAvailable', CoinController.getAvailable);
-router.get('/image/:image', CoinController.getImage);
+router.get('/coin', Auth, CoinController.getAll);
+router.get('/id/:id', Auth, CoinController.getId);
+router.get('/name/:name', Auth, CoinController.getName);
+router.get('/country/:country', Auth, CoinController.getCountry);
+router.get('/value/:value', Auth, CoinController.getValue);
+router.get('/value_us/:value_us', Auth, CoinController.getVU);
+router.get('/year/:year', Auth, CoinController.getYear);
+router.get('/review/:review', Auth, CoinController.getReview);
+router.get('/available/:isAvailable', Auth, CoinController.getAvailable);
+router.get('/image/:image', Auth, CoinController.getImage);
 router.post('/signup', AuthController.emailSignup);
-router.post('/signin', AuthController.emailLogin)
-router.get('/private', Auth, (req, res) => { res.status(200).send({ message: 'Estas dentro we' }) });
+//router.post('/signin', AuthController.emailLogin)
 
 // Update
 router.put('/id/:id', CoinController.update);
